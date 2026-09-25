5b422e530334a9a89d339ddfb5f28d29ea9af473
Amirkhan <wompwompp597@gmail.com>
2026-09-25 23:38:16 +0600

    DZ4

--- bin/Month2/m2hw1/hw1m2/flutter_lesson4/lib/main.dart
+++ bin/Month2/m2hw1/hw1m2/flutter_lesson4/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lesson4/myapp.dart';
+import 'package:flutter_lesson4/newsapp.dart';
 import 'pageview.dart';
 import 'controlwork.dart';
 
-void main() => runApp(FeedbackScreen());
+void main() => runApp(NewsApp());
--- bin/Month2/m2hw1/hw1m2/flutter_lesson4/lib/newsapp.dart
+++ bin/Month2/m2hw1/hw1m2/flutter_lesson4/lib/newsapp.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
-class NewsApp extends StatelessWidget{
+class NewsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.teal,
-        scaffoldBackgroundColor: Colors.grey[50],
-        textTheme: TextTheme(
+        scaffoldBackgroundColor: Colors.grey,
+        textTheme: const TextTheme(
           titleLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          titleMedium: TextStyle(fontSize: 14))
+          titleMedium: TextStyle(fontSize: 14),
+        ),
       ),
       debugShowCheckedModeBanner: false,
       home: NewsHome(),
@@ -18,30 +19,104 @@ class NewsApp extends StatelessWidget{
   }
 }
 
-class NewsHome extends StatelessWidget{
+class NewsHome extends StatelessWidget {
   final List<Map<String, String>> news = List.generate(20, (i) => {
     'title': 'Заголовок новости ${i + 1}',
     'subtitle': 'описание новости ${i + 1}',
     'image': 'https://picsum.photos/seed/news$i/400/200'
   });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Лента новостей')),
-      body: ListView.builder(itemBuilder: (context, index){final item = news[index];
-      return Card(margin: EdgeInsets.symmetric(vertical: 8),
-      elevation: 1, child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [Image.network(item['image']!, height: 160, width: double.infinity, fit: BoxFit.cover,),
-        Padding(padding: EdgeInsets.all(16), child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Text(item['title']!, style: Theme.of(context).textTheme.titleLarge,),
-          SizedBox(height: 8,),
-          Text('')
-        ],),)],
+      appBar: AppBar(title: const Text('Лента новостей')),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: news.length,
+        itemBuilder: (context, index) {
+          final item = news[index];
+          
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24),
+            ),
+            clipBehavior: Clip.antiAlias,
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.04),
+                    blurRadius: 16,
+                    offset: const Offset(0, 6),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Image.network(
+                        item['image']!,
+                        height: 160,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned.fill(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Colors.transparent,
+                                Color.fromRGBO(0, 0, 0, 0.7),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 16,
+                        left: 16,
+                        right: 16,
+                        child: Text(
+                          item['title']!,
+                          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                offset: const Offset(0, 1),
+                                blurRadius: 4,
+                                color: Color.fromRGBO(0, 0, 0, 0.4),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          item['subtitle']!,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
-      );
-      }, padding: EdgeInsets.all(16), itemCount: news.length,),
     );
   }
-
 }
